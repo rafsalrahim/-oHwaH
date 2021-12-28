b62f2d9ad541d3064a8b8f7b5f18c1e3c1bc844a
rafsalrahim <rafsalkrahim@gmail.com>
2021-12-28 21:13:39 +0530

    contract updayte

--- Ethereum/SmartContracts/simple_wallet.sol
+++ Ethereum/SmartContracts/simple_wallet.sol
@@ -1,10 +1,14 @@
 pragma solidity >=0.7.0 <0.9.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
 
-contract simple_wallet is Ownable {
+contract Allowance is Ownable{
 
+    event AllowanceChanged(address indexed _to, address indexed _from, uint _oldAmt, uint _newAmt);
 mapping(address =>uint) public allowance;
+
     function addAllowance(address _who, uint _amount) public onlyOwner{
+        emit AllowanceChanged(_who,msg.sender,allowance[_who],_amount);
         allowance[_who]=_amount;
     }
 
@@ -17,16 +21,28 @@ mapping(address =>uint) public allowance;
         _;
     }
     function reduceAllowance(address _who, uint _amount) internal{
+    emit AllowanceChanged(_who,msg.sender,allowance[_who],allowance[_who] - _amount);
     allowance[_who] -= _amount;
     }
+
+}
+
+
+
+contract simple_wallet is Allowance {
+
+    event MonrySend(address indexed _benificiary, uint _amount);
+    event MoneyRecieved(address indexed _from, uint _amount);
     function withdrawMoney(address payable _to, uint _amount) public ownerorAllowed(_amount){
+        require(_amount <= address(this).balance, "Not enough fund in Smart contract");
         if(!isOwner()){
         reduceAllowance( msg.sender,  _amount);
         }
+        emit MonrySend(_to,_amount);
         _to.transfer(_amount);
     }
 
     fallback() external payable {
-
+        emit MoneyRecieved(msg.sender,msg.value);
     }
 }
