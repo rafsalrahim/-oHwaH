f3add18702b948f72757744120d5c8787daa987a
rafsalrahim <rafsalkrahim@gmail.com>
2022-01-15 13:25:11 +0530

    2

---
+++ Python/Random/substringdelamma.py
@@ -0,0 +1,35 @@
+from imghdr import tests
+
+
+def test_1(string=""):
+
+    # initializing the substring
+    substring = ""
+    testList = []
+    initial = 0
+
+    while initial < len(string) and len(string):
+        for i in range(initial, len(string)):
+            substring += string[i]
+
+            # checking conditions
+            if substring.count(string[i]) > 1:
+                testList.append(substring[:-1])
+                initial += 1
+                substring = ""
+                break
+    maxi = ""
+
+    for word in testList:
+
+        if len(word) > len(maxi):
+            maxi = word
+
+    if len(maxi) < 3:
+        return "-1"
+    else:
+        return maxi
+
+
+# Driver code
+print(test_1("character"))
