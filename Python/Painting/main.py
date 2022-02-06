c378bfa4a41bddff1f6f9d98e02f88243ef0587e
rafsalrahim <rafsalkrahim@gmail.com>
2022-02-06 21:44:30 +0530

    2nd push

--- Python/Painting/main.py
+++ Python/Painting/main.py
@@ -1,9 +1,11 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+import turtle as t
 import random
 
-picachu = Turtle()
+picachu = t.Turtle()
 picachu.shape("arrow")
 picachu.color("purple")
+picachu.speed("fastest")
 
 # pgm 1 dashed - squre
 for _ in range(4):
@@ -25,12 +27,41 @@ color_pallet = ["navy", "spring green", "gold", "tomato", "light coral",
 # shapes
 for i in range(3, 10):
     picachu.color(random.choice(color_pallet))
-    for j in range(1, i):
+    for j in range(1, i+1):
         picachu.forward(100)
         picachu.right(360/i)
 
-# random walk
 
+# random walk
+direction = [0, 90, 180, 270]
+t.colormode(255)
+
+
+def randCol():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
+
+
+for _ in range(300):
+    picachu.color(randCol())
+    picachu.pensize(10)
+    picachu.forward(30)
+    picachu.setheading(random.choice(direction))
+
+
+# spiral graphed
+
+
+def spiral(size):
+    for _ in range(int(360/size)):
+        picachu.color(randCol())
+        picachu.setheading(picachu.heading()+size)
+        picachu.circle(100)
+
+
+spiral(3)
 
 screen = Screen()  # class to make sure the window stays
 screen.exitonclick()  # function self explanatory
