0ca5d7dc47ff7bec8f2e78cdb3ad40c3b151beac
rafsalrahim <rafsalkrahim@gmail.com>
2022-03-03 21:58:50 +0530

    marketplace creating halfway

--- Ethereum/nft-marketplace/contracts/nft_market.sol
+++ Ethereum/nft-marketplace/contracts/nft_market.sol
@@ -8,4 +8,35 @@ contract NFTMarket is ReentrancyGuard {
     using Counters for Counters.Counter;
     Counters.Counter private _itemIds;
     Counters.Counter private _itemIds;
+
+    address payable owner;
+    uint256 listPrice = 0.025 ether;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    struct MarketItem {
+        uint256 itemId;
+        address nftContractor;
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }
+    mapping(uint256 => MarketItem) private idToMarketitem;
+    event MarkwtItemCreated(
+        uint256 indexed itemId,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool soled
+    );
+
+    function getListPrice() public view returns (uint256) {
+        return listPrice;
+    }
 }
