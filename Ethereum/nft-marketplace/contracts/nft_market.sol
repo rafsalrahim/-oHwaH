406efc0784aa7dbe88150cc88d7b398cd29647e1
rafsalrahim <rafsalkrahim@gmail.com>
2022-03-03 22:16:29 +0530

    token and market place contract midway

--- Ethereum/nft-marketplace/contracts/nft_market.sol
+++ Ethereum/nft-marketplace/contracts/nft_market.sol
@@ -39,4 +39,29 @@ contract NFTMarket is ReentrancyGuard {
     function getListPrice() public view returns (uint256) {
         return listPrice;
     }
+
+    function createMarketItem() public view returns (address nftContract,
+    uint256 tokenId,
+    uint256 price) public payable nonReentrant{ //non rentrent for preventing re entry
+    require(price >= 0,"price must be > 0");
+    require(msg.value == listPrice,"price  must be equal ti listing price");
+
+    _itemIds.increment();
+    uint256 itemId= _itemIds.current();
+
+    idToMarketitem[itemId] =MarketItem(
+        itemId,
+        nftContract,
+        tokenId,
+        payable(msg.sender),
+        payable(address(0)),
+        price,
+        false);
+
+    IERC721(nftContract).transferFrom(msg.sender,address(this),tokenId);
+
+    emit MarketItemCreated(itemId,nftContract,tokenId,msg.sender,address(0),price,false);
+
+    }
+
 }
